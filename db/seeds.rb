afe9a0ca55933c131728073e3d05f313f09217c3
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-02-26 20:16:44 -0500

    use FFaker gem to add more contacts randomizing first_name, last_name, email, and phone_number

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # first name
+# FFaker::Name.first_name
+# # last name
+# FFaker::Name.last_name
+# # phone number
+# FFaker::PhoneNumber.phone_number
+# # email
+# FFaker::Internet.email
+
+50.times do 
+   contact1 = Contact.new(first_name:FFaker::Name.first_name, last_name:FFaker::Name.last_name, phone_number:FFaker::PhoneNumber.phone_number, email:FFaker::Internet.email)
+  contact1.save
+end
