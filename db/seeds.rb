1e7badb2f5773e98daeb7ebd22815442d23d26b4
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-02 21:02:42 -0500

    updated code so that email is first_name.last_name@gmail.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 # FFaker::Internet.email
 
 50.times do 
-   contact1 = Contact.new(first_name:FFaker::Name.first_name, last_name:FFaker::Name.last_name, phone_number:FFaker::PhoneNumber.phone_number, email:FFaker::Internet.email)
+    first_name = FFaker::Name.first_name
+    last_name = FFaker::Name.last_name
+    email = "#{first_name}.#{last_name}@gmail.com"
+   contact1 = Contact.new(first_name:first_name, last_name:last_name, phone_number:FFaker::PhoneNumber.phone_number, email:email)
   contact1.save
 end
