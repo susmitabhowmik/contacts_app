150da29922a3e2aa2a8013d0772dd450f5bd1afb
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-06 20:42:28 -0500

    update update and create in controller to include middle_name and bio

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -12,10 +12,12 @@ class Api::ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(
-      first_name: params[:first_name] || "Bob",
-      last_name: params[:last_name] || "Smith",
-      email: params[:email] || "defaultemail@gmail.com",
-      phone_number: params[:phone_number] || 10)
+      first_name: params[:first_name],
+      last_name: params[:last_name] ,
+      email: params[:email],
+      phone_number: params[:phone_number],
+      middle_name: params[:middle_name],
+      bio: params[:bio])
     @contact.save
     render 'show.json.jbuilder'
   end
@@ -26,7 +28,9 @@ class Api::ContactsController < ApplicationController
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
-    @contact.phone_number = params[:phone_number] || @contact.phone_number      
+    @contact.phone_number = params[:phone_number] || @contact.phone_number    
+    @contact.middle_name = params[:middle_name] || @contacts.middle_name
+    @contact.bio = params[:bio] || @contacts.bio  
     @contact.save
     render 'show.json.jbuilder'
   end
--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -1,8 +1,8 @@
 json.id contact.id
 json.first_name contact.first_name
+json.middle_name contact.middle_name
 json.last_name contact.last_name
 json.phone_number contact.phone_number
 json.email contact.email
 json.updated_at contact.updated_at
-json.middle_name contact.middle_name
 json.bio contact.bio
