2859ad4afe50ae279812f5bb0c716b3cbe3e5fd0
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-07 20:18:34 -0500

    add validation to have email be unique

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   validates :first_name, presence:true
   validates :last_name, presence:true
-  
+  validates :email, uniqueness:true
   def friendly_updated_at
     created_at.strftime("%b %e, %l:%M %p")
   end
